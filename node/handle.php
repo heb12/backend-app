a2e57e963ed02cd0bf4070bcef5f81a3cc1111b9
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-25 17:10:41 -0400

    Update handle.php

--- node/handle.php
+++ node/handle.php
@@ -1,5 +1,5 @@
 <?php
-$myfile = fopen($_GET["book"]+$_GET["chapter"]+".json", "r") or die("Unable to open file!");
-echo fread($myfile,filesize($_GET["book"]+$_GET["chapter"]+".json"));
+$myfile = fopen($_GET["book"]+".json", "r") or die("Unable to open file!");
+echo fread($myfile,filesize($_GET["book"]+".json"));
 fclose($myfile);
 ?>
