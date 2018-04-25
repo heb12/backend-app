8b5664d81e25f5deb9650f7eb0fce11603e41494
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-25 17:10:12 -0400

    Update script.js

--- node/script.js
+++ node/script.js
@@ -1,1 +1,12 @@
-
+function get(book) {           
+$.noConflict
+            function require(url) {
+                var rtrn
+             jQuery.ajax({async:false,url: url, success: function(result){
+                if(url.search(".json")<-1) {rtrn= JSON.parse(result);} else if(url.search(".js")!==-1) {rtrn= eval("function() {"+result+"}");} else {
+            rtrn= result;
+                }
+        }});return rtrn;
+            }
+var json = require("handle.php?book="+book);
+}
